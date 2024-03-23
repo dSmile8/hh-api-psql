64fe5c177bf040852af6954a244f6df8794a90c4
Сергей Арефьев <Simpson6515@yandex.ru>
2024-03-24 01:32:48 +0500

    completed work with API and file_manage

--- src/user_iteraction.py
+++ src/user_iteraction.py
@@ -1,16 +1,26 @@
 from api_connect import HeadHunterAPI
 from work_with_file import WorkWithFile
-from utils import print_employers
+from utils import print_employers, create_company_list, sort_data
 from pathlib import Path
 
-DATA_DIR_VACANCIES = Path(__file__).parent.parent.joinpath('data', 'hh.json')
+DATA_DIR_VACANCIES = Path(__file__).parent.parent.joinpath('data', 'hh_vacancies.json')
+DATA_DIR_VAC_COMP = Path(__file__).parent.parent.joinpath('data', 'hh_vac_company.json')
+DATA_DIR_SORT = Path(__file__).parent.parent.joinpath('data', 'hh_vac_company_sorted.json')
 
 vacancy_name = input('Введите интересующую вас вакансию, например: "водитель"\n').lower()
-hh_api = HeadHunterAPI(vacancy_name)
-vacancies = WorkWithFile(DATA_DIR_VACANCIES).save_to_json(hh_api.get_vacancies())  # Получаем список вакансий по запросу
-# и сохраняем их в файл
-print_employers(DATA_DIR_VACANCIES)  # Вывод на экран названия компаний и их id
+hh_vacancies = HeadHunterAPI(vacancy_name)
+file_worker = WorkWithFile()
+file_worker.save_to_json(DATA_DIR_VACANCIES, hh_vacancies.get_vacancies())  # Получаем список вакансий
+# по запросу и сохраняем их в файл
 
+print_employers(file_worker.data_from_json(DATA_DIR_VACANCIES))  # Вывод на экран названия компаний и их id
+company_list = create_company_list()  # Создаем список компаний
+hh_vacancies_company = HeadHunterAPI(vacancy_name, company_list)  # Делаем запрос к hh.ru со списком
+# интересующих компаний
 
+file_worker.save_to_json(DATA_DIR_VAC_COMP, hh_vacancies_company.get_vacancies())  # Сохраняем
+# в файл
 
-# print(hh_api.get_vacancies())
+file_worker.save_to_json(DATA_DIR_SORT, sort_data(file_worker.data_from_json(DATA_DIR_VAC_COMP)))  # Сортируем
+# данные и сохраняем их в файл
+
--- src/work_with_file.py
+++ src/work_with_file.py
@@ -1,19 +1,15 @@
 from abc import ABC, abstractmethod
 
 import json
-from pathlib import Path
-from api_connect import HeadHunterAPI
-
-DATA_DIR_VACANCIES = Path(__file__).parent.parent.joinpath('data', 'hh.json')
 
 
 class BaseWorkWithFile(ABC):
     @abstractmethod
-    def save_to_json(self, data_):
+    def save_to_json(self, file, data_):
         pass
 
     @abstractmethod
-    def data_from_json(self):
+    def data_from_json(self, file):
         pass
 
     @abstractmethod
@@ -22,21 +18,20 @@ class BaseWorkWithFile(ABC):
 
 
 class WorkWithFile(BaseWorkWithFile):
+    def __init__(self):
+        self.data_ = None
 
-    def __init__(self, file):
-        self.file = file
-
-    def save_to_json(self, data_):
+    def save_to_json(self, file, data_):
         """Сохраняет данные в файл"""
 
         self.data_ = data_
-        with open(self.file, "w", encoding="utf-8") as f:
+        with open(file, "w", encoding="utf-8") as f:
             json.dump(self.data_, f, ensure_ascii=False)
 
-    def data_from_json(self):
+    def data_from_json(self, file):
         """Получает данные из файла"""
 
-        with open(self.file, "r", encoding="utf-8") as f:
+        with open(file, "r", encoding="utf-8") as f:
             data_dict = json.load(f)
             return data_dict
 
@@ -45,11 +40,4 @@ class WorkWithFile(BaseWorkWithFile):
 
 
 if __name__ == '__main__':
-    hh = HeadHunterAPI('Python', [4305039])
-    WorkWithFile(DATA_DIR_VACANCIES).save_to_json(hh.get_vacancies())
-    # print(WorkWithFile(DATA_DIR_VACANCIES).data_from_json())
-
-    data = WorkWithFile(DATA_DIR_VACANCIES).data_from_json()
-    for d in data['items']:
-        print(f"{d['employer']['name']}\n{d['employer']['id']}\n{d['employer']['alternate_url']}\n")
-
+    pass
