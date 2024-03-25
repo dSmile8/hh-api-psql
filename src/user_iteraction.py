0e9d4d34532f5d49712b3f791bbada71f0d8297d
Сергей Арефьев <Simpson6515@yandex.ru>
2024-03-25 16:29:43 +0500

    added create db, table and fill db

--- src/user_iteraction.py
+++ src/user_iteraction.py
@@ -1,6 +1,8 @@
 from api_connect import HeadHunterAPI
 from work_with_file import WorkWithFile
 from utils import print_employers, create_company_list, sort_data
+from db_creator import DBCreator
+
 from pathlib import Path
 
 DATA_DIR_VACANCIES = Path(__file__).parent.parent.joinpath('data', 'hh_vacancies.json')
@@ -10,6 +12,7 @@ DATA_DIR_SORT = Path(__file__).parent.parent.joinpath('data', 'hh_vac_company_so
 vacancy_name = input('Введите интересующую вас вакансию, например: "водитель"\n').lower()
 hh_vacancies = HeadHunterAPI(vacancy_name)
 file_worker = WorkWithFile()
+db_creat = DBCreator()
 file_worker.save_to_json(DATA_DIR_VACANCIES, hh_vacancies.get_vacancies())  # Получаем список вакансий
 # по запросу и сохраняем их в файл
 
@@ -23,4 +26,9 @@ file_worker.save_to_json(DATA_DIR_VAC_COMP, hh_vacancies_company.get_vacancies()
 
 file_worker.save_to_json(DATA_DIR_SORT, sort_data(file_worker.data_from_json(DATA_DIR_VAC_COMP)))  # Сортируем
 # данные и сохраняем их в файл
+db_creat.create_database()  # Создаем БД
+db_creat.create_table('vacancies')  # Создаем таблицу
+db_creat.fill_the_table('vacancies', DATA_DIR_SORT)  # Заполняем таблицу
+
+
 
