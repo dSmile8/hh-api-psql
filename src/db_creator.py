510afb89a1d2cc8c68a9fb61d962ee891af04d2e
Сергей Арефьев <Simpson6515@yandex.ru>
2024-03-25 16:28:40 +0500

    added func fill_the_table

---
+++ src/db_creator.py
@@ -0,0 +1,93 @@
+import psycopg2
+from pathlib import Path
+import json
+
+DATA_DIR_VACANCIES = Path(__file__).parent.parent.joinpath('data', 'hh_vacancies.json')
+DATA_DIR_VAC_COMP = Path(__file__).parent.parent.joinpath('data', 'hh_vac_company.json')
+DATA_DIR_SORT = Path(__file__).parent.parent.joinpath('data', 'hh_vac_company_sorted.json')
+
+
+class DBCreator:
+    """Создает БД, таблицу и заполняет ее"""
+    def __init__(self, db_name='hh_ru'):
+
+        self.db_name = db_name
+        self.conn = None
+        self.params = {'host': 'localhost',
+                       'port': '5432',
+                       'database': self.db_name,
+                       'user': 'postgres',
+                       'password': 'asg6515ZX'}
+
+    def create_database(self) -> None:
+        """Создает базу данных"""
+        self.conn = psycopg2.connect(host='localhost', port='5432', database='postgres',
+                                     user='postgres', password='asg6515ZX')
+        try:
+            cursor = self.conn.cursor()
+            self.conn.autocommit = True
+            cursor.execute(f"CREATE DATABASE {self.db_name}")
+            cursor.close()
+            print(f"База данных {self.db_name} успешно создана!")
+        except (Exception, psycopg2.DatabaseError) as error:
+            print("Ошибка при создании базы данных:", error)
+        finally:
+            self.conn.close()
+
+    def create_table(self, table_name: str) -> None:
+        """Создаёт таблицу в БД"""
+        self.conn = psycopg2.connect(**self.params)
+        try:
+            with self.conn as conn:
+                with conn.cursor() as cur:
+                    cur.execute(f'CREATE TABLE {table_name}'
+                                '(vacancy_id int PRIMARY KEY,'
+                                'vacancy_name varchar(100),'
+                                'company_name varchar(150),'
+                                'city varchar(100),'
+                                'salary_from int,'
+                                'salary_to int,'
+                                'currency varchar(5),'
+                                'schedule varchar(50),'
+                                'experience varchar(50),'
+                                'requirement text,'
+                                'published timestamp,'
+                                'url text'
+                                ')')
+                    cur.close()
+                    print(f"Таблица {table_name} успешно создана!")
+
+        except(Exception, psycopg2.DatabaseError) as error:
+            print(error)
+        finally:
+            if self.conn is not None:
+                self.conn.close()
+
+    def fill_the_table(self, table_name: str, data_f) -> None:
+        """Заполняет созданную таблицу в БД"""
+        self.conn = psycopg2.connect(**self.params)
+        try:
+            with self.conn as conn:
+                with conn.cursor() as cur:
+                    with open(data_f, 'r', encoding='UTF-8') as file:
+                        data = json.load(file)
+                        for d in data:
+                            cur.execute(
+                                f'INSERT INTO "vacancies" VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)',
+                                (d['vacancy_id'], d['vacancy_name'], d['company_name'],
+                                 d['city'], d['salary_from'], d['salary_to'], d['currency'], d['schedule'],
+                                 d['experience'], d['requirement'], d['published'], d['url']))
+                    cur.close()
+                    print(f"Таблица {table_name} успешно заполнена!")
+        except(Exception, psycopg2.DatabaseError) as error:
+            print(error)
+        finally:
+            if self.conn is not None:
+                self.conn.close()
+
+
+if __name__ == '__main__':
+    dbm = DBCreator()
+    dbm.create_database()
+    dbm.create_table('vacancies')
+    dbm.fill_the_table('vacancies')
